f62e36460cfc6b959a54d60a41ef33ff5436f663
Chuka Ebi <chuka@apples-MacBook-Pro.local>
2021-01-10 22:42:20 +0000

    ...

--- pages/index.js
+++ pages/index.js
@@ -28,7 +28,7 @@ export default function Home({ allPostsData }) {
           </a>
         </p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{" "}
+          (This is a sample - you’ll be building a site like this on{" "}
           <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
         </p>
       </section>
